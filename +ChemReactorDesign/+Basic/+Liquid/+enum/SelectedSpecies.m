89316d6fe56b1f806df91d3fd106c206e80cc303
Klaus Schnitzlein <linoth@arcor.de>
2025-11-18 11:52:44 +0100

    use Gibbs energy instead of entropy as domain parameter

--- +ChemReactorDesign/+Basic/+Liquid/+enum/SelectedSpecies.m
+++ +ChemReactorDesign/+Basic/+Liquid/+enum/SelectedSpecies.m
@@ -1,13 +1,15 @@
 classdef SelectedSpecies < int32
 	enumeration
-		S1	(1)
-		S2	(1)
+		S1	(12)
+		S2	(13)
+		S3	(1)
 	end
 	methods (Static)
 		function map = displayText()
 			map = containers.Map;
-			map('S1') = 'H2O';
-			map('S2') = 'H2O';
+			map('S1') = 'A1';
+			map('S2') = 'A2';
+			map('S3') = 'H2O';
 		end
 	end
 end
